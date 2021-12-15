cf049b8a6f3b7fbd33e93a408267a60a1558c0f7
rumeshudash <rumesh.udas@gmail.com>
2021-12-15 14:10:32 +0530

    Updates

---
+++ precache-manifest.710881af10cf4980ac2e79d8f8e9edae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6340e44ea5de1d4ff76db6a25cfc934c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8be129511e1a6e72ac0f",
+    "url": "./static/css/main.dc661683.chunk.css"
+  },
+  {
+    "revision": "5e3b0fb06b8d59d3e53c",
+    "url": "./static/js/2.f3fe4b69.chunk.js"
+  },
+  {
+    "revision": "37fac929e9ec5b38bfd83c5fc6405d00",
+    "url": "./static/js/2.f3fe4b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be129511e1a6e72ac0f",
+    "url": "./static/js/main.132fce0c.chunk.js"
+  },
+  {
+    "revision": "d7d732b97995adc45d26",
+    "url": "./static/js/runtime-main.0a3659c3.js"
+  }
+]);
